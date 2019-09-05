653c85cb468e19d13c66b7489995262afbe35aa8
Thomas <denhollander.thomas@gmail.com>
2019-09-05 21:19:25 +0200

    Add benchmarks

---
+++ benches/zxcvbn.rs
@@ -0,0 +1,14 @@
+use criterion::{criterion_group, criterion_main};
+use criterion::Criterion;
+use criterion::black_box;
+
+use zxcvbn::zxcvbn;
+
+pub fn bench_zxcvbn(c: &mut Criterion) {
+    c.bench_function("zxcvbn", |b| b.iter(|| {
+        zxcvbn(black_box("r0sebudmaelstrom11/20/91aaaa"), &[])
+    }));
+}
+
+criterion_group!(benches, bench_zxcvbn);
+criterion_main!(benches);
---
+++ benches/zxcvbn_unicode.rs
@@ -0,0 +1,14 @@
+use criterion::{criterion_group, criterion_main};
+use criterion::Criterion;
+use criterion::black_box;
+
+use zxcvbn::zxcvbn;
+
+pub fn bench_zxcvbn_unicode(c: &mut Criterion) {
+    c.bench_function("zxcvbn_unicode", |b| b.iter(|| {
+        zxcvbn(black_box("𐰊𐰂𐰄𐰀𐰁"), &[])
+    }));
+}
+
+criterion_group!(benches, bench_zxcvbn_unicode);
+criterion_main!(benches);
