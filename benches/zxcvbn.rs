8e42d5b6ee7014698e6d93325f0a4ab67c5179d4
Thomas <denhollander.thomas@gmail.com>
2019-09-05 21:23:37 +0200

    cargo fmt

--- benches/zxcvbn.rs
+++ benches/zxcvbn.rs
@@ -1,13 +1,13 @@
-use criterion::{criterion_group, criterion_main};
-use criterion::Criterion;
 use criterion::black_box;
+use criterion::Criterion;
+use criterion::{criterion_group, criterion_main};
 
 use zxcvbn::zxcvbn;
 
 pub fn bench_zxcvbn(c: &mut Criterion) {
-    c.bench_function("zxcvbn", |b| b.iter(|| {
-        zxcvbn(black_box("r0sebudmaelstrom11/20/91aaaa"), &[])
-    }));
+    c.bench_function("zxcvbn", |b| {
+        b.iter(|| zxcvbn(black_box("r0sebudmaelstrom11/20/91aaaa"), &[]))
+    });
 }
 
 criterion_group!(benches, bench_zxcvbn);
