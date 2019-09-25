7c50c2f4e7f336ca1ba24a35e1ef184fa3f3f864
Thomas den Hollander <ThomasdenH@users.noreply.github.com>
2019-09-25 19:00:45 +0200

    Add benchmarks (#29)

---
+++ benches/zxcvbn_unicode.rs
@@ -0,0 +1,14 @@
+use criterion::black_box;
+use criterion::Criterion;
+use criterion::{criterion_group, criterion_main};
+
+use zxcvbn::zxcvbn;
+
+pub fn bench_zxcvbn_unicode(c: &mut Criterion) {
+    c.bench_function("zxcvbn_unicode", |b| {
+        b.iter(|| zxcvbn(black_box("𐰊𐰂𐰄𐰀𐰁"), &[]))
+    });
+}
+
+criterion_group!(benches, bench_zxcvbn_unicode);
+criterion_main!(benches);
