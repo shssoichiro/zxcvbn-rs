b3fc29ac0df7552f4be9aa5fc1454548c3985822
Josh Holmer <jholmer@activecampaign.com>
2020-01-10 12:51:20 -0500

    Remove intermittently breaking test assertion
    Due to changing implementations of float rounding
    between Rust/LLVM versions,
    this test has broken more than once on this assert.
    It seems reasonable to remove this assert,
    given that with such a large score,
    the exact number is less important than the estimate (log10).

--- src/lib.rs
+++ src/lib.rs
@@ -179,7 +179,6 @@ mod tests {
     fn test_zxcvbn() {
         let password = "r0sebudmaelstrom11/20/91aaaa";
         let entropy = zxcvbn(password, &[]).unwrap();
-        assert_eq!(entropy.guesses, 490_970_150_656_000);
         assert_eq!(entropy.guesses_log10 as u16, 14);
         assert_eq!(entropy.score, 4);
         assert!(!entropy.sequence.is_empty());
